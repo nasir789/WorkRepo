12f9a6caa6c9bde8ad6cf3d663c977baac1a0624
Araib Nasir <araibn@gmail.com>
2016-04-18 22:02:52 -0400

    made sure data was passing as JSON using app.post in app.js, connected mongoDb with mongoose, installed bodyparser, created and defined Schema

--- loginsetup/app.js
+++ loginsetup/app.js
@@ -1,7 +1,28 @@
+var bodyParser = require('body-parser');
 var express = require('express');
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var User = mongoose.model('User', new Schema({
+  id: ObjectId,
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true},
+  password: String,
+}));
 
 var app = express();
 app.set('view engine', 'jade');
+app.locals.pretty = true;
+
+//connect to mongo
+mongoose.connect('mongodb://localhost/newauth');
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true}));
+
 
 app.get('/', function(req,res){
   res.render('index.jade');
@@ -9,6 +30,10 @@ app.get('/', function(req,res){
 
 app.get('/register', function(req,res){
   res.render('register.jade');
+});
+
+app.post('/register', function(req,res){
+  res.json(req.body);
 });
 
 app.get('/login', function(req,res){
