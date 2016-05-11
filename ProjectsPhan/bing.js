3b060d51a98fc911c8f983c4d1ddfc51b9d36eaa
Araib Nasir <araibn@gmail.com>
2016-05-10 20:50:14 -0400

    using vanillaJS to setup bing queries, need to look at API Documentation to fill form in again via phantom

--- ProjectsPhan/bing.js
+++ ProjectsPhan/bing.js
@@ -4,11 +4,27 @@ var casper = require('casper').create({
   clientScripts:[];
 });
 
+//global links variable, to be pushed into an array
 var links = [];
 
+//targeting 'b_algo' class, hence '.' and within class target 'a' element
 function getLinks() {
   var links = document.querySelectorAll('.b_algo a');
   return Array.prototype.map.call(links, function(e){
     return e.getAttribute('href');
   });
 };
+
+casper.start('http://bing.com/', function(){
+  this.fill('form[action="/search]',{
+    q: 'casperjs'
+  }, true);
+});
+
+//aggregate results of our search by passing in function 'getLinks'
+casper.then(function(){
+  //setting global variables, evaluate getLinks 
+  links = this.evaluate(getLinks);
+})
+
+casper.run();
