eecbc2cf4364a880e662ec41d90f22585407a6c1
Araib Nasir <araibn@gmail.com>
2016-06-29 23:36:39 -0400

    NC , addition to console.log

--- ReactRelated/dropdown/src/list-item.jsx
+++ ReactRelated/dropdown/src/list-item.jsx
@@ -4,7 +4,7 @@ var React = require('react');
 module.exports = React.createClass({
   handleClick: function(){
     this.props.whenItemClicked(this.props.item);
-    console.log('i was clicked');
+    console.log('i was clicked!');
   },
   render: function(){
     return <li className={this.props.className}>
