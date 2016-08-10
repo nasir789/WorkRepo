028ccfa026d4cee8b77b6754fd9e4d8b75394610
Araib Nasir <araibn@gmail.com>
2016-08-10 07:13:56 -0400

    changes made to flland page, need to review/clean up misc code, and implement jquery to contactUs page

--- fllandpage/app.js
+++ fllandpage/app.js
@@ -1,16 +1,115 @@
 
-for (var i=0; i<100; i++){
-  document.write("number" + i + "</br>");
-}
 
+//OnLoad
 function showThis(){
   $("#buttonOne").fadeIn("medium");
   $(".blackOverLay").fadeIn(4000);
+  $(".firstLine").fadeIn();
+  $(".holderMaitre").hide();
+  $("#backFromWin").hide();
+  //
+  $(".chatBox").hide(8000);
+  $(".adviceBox").hide(8000);
+  $(".talkBox").hide(8500);
+  $(".moreBox").hide(11500);
+  $(".chatBox").fadeIn(8000);
+  $(".adviceBox").fadeIn(8000);
+  $(".talkBox").fadeIn(9500);
+  $(".moreBox").fadeIn(12500);
+  $(".fLTitleLogo").fadeIn(8000);
+
 };
 
 
+
+//Will fade in action call
 
 setTimeout(
   function()
   { showThis()
   }, 5000);
+
+//Exit out overlay
+
+function closeThis(){
+  $(".imageContainer").slideUp(1000);
+  $("#buttonOne").fadeOut("medium");
+  $(".blackOverLay").fadeOut(1000);
+};
+
+//Render Win/Maitre
+function winThis(){
+  $(".imageContainer").fadeOut("medium");
+  $(".holderMaitre").fadeIn("medium");
+  $(".blackOverLayTwo").fadeIn("medium");
+  $("#backFromWin").show();
+  $(".imageHold").fadeOut("medium");
+  $(".guideInfoContent").hide("medium");
+  $(".firstLine").hide("medium");
+  $(".logoImage").hide("medium");
+  $(".serviceContainer").hide("medium");
+  $(".fillerSpace").hide("medium");
+  $(".storeLinks").hide("medium");
+  $(".contactUs").fadeOut("medium");
+  $(".contactUsX").fadeOut("medium");
+
+};
+
+
+$(function weDontKnow(){
+  $(".contactInfo").hide();
+  $(".buttonHolder").hide();
+});
+
+$(function contactHit(){
+  console.log('what');
+  $(".contactUs").on("click", function contactHit(){
+    $(".blackOverLay").fadeOut(3000);
+    $(".contactInfo").fadeIn(4000);
+    $(".contactUs").fadeOut(3000);
+    $(".contactContent").fadeIn(3000);
+  });
+});
+
+
+
+//Back Functionality
+
+
+
+$(function goBackNow(){
+  console.log('hitting');
+  $("#backButton").on("click", function goBackNow(){
+    $(".contactInfo").fadeOut(2000);
+    $(".contactContent").fadeOut(2000);
+    //Unresolved
+    $("#buttonOne").fadeIn("medium");
+    $(".blackOverLay").fadeIn(4000);
+    $(".contactUs").fadeIn(4000);
+
+  });
+});
+
+$(function goBackFromWin(){
+  console.log('operational');
+  $("#backFromWin").on("click", function goBackFromWin(){
+    //show Main Menu
+
+    //
+    $(".blackOverLayTwo").hide(2000);
+    $(".holderMaitre").hide(2000);
+    $(".imageContainer").fadeIn(4000);
+    $(".blackOverLay").fadeIn(4000);
+    $(".imageHold").fadeIn(4000);
+    $(".serviceContainer").fadeIn(4000);
+    $(".contactDiv").fadeIn(4000);
+    $(".contactUs").fadeIn(4000);
+    $(".firstLine").fadeIn(4000);
+    $(".storeLinks").fadeIn(4000);
+    $(".item").fadeIn(4000);
+    $(".logoImage").fadeIn(4000);
+    $(".fillerSpace").fadeIn(4000);
+    $("#backFromWin").hide();
+
+  });
+});
---
+++ fllandpage/winapp.js
@@ -0,0 +1,35 @@
+
+<script type="text/javascript">
+  window.Maitre = {
+    uuid: "xxxxxxxx",
+    host: "http://mywebsite.com",
+    source: "product_hunt",
+    test_mode: false,
+    require_name: true,
+    require_leaderboard: false,
+    labels: {
+        header: "First 20 people win a free ticket",
+        email_label: "Email",
+        name_label: "Name",
+        name_placeholder: "Your Name",
+        email_placeholder: "Your email address",
+        status_label: "Check status",
+        submit_button: "Submit",
+        check_position_label: "Check position",
+        submitting_label: "Submitting...",
+        back_label: "Back",
+        success_main_header: "Thank you!",
+        people_front: "People in front of you",
+        people_behind: "People behind you",
+        instructions: "Share your personal link."
+    },
+    alerts: {
+        subcriber_not_found: "Email not found",
+        subscriber_accepted: "You have already been accepted.",
+        subscriber_not_verified: "You must confirm your email first.",
+        form_incomplete: "Something is missing. Please check the form.",
+        server_problem: "We are experiencing some issues on our server."
+    }
+  };
+
+</script>
